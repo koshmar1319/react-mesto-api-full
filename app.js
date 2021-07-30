b56c35957a0d8183daf63a4c3ffae4fa9565248f
Kirill Komarkov <koshmar1319@gmail.com>
2021-07-30 19:52:34 +0300

    pr13 fix code after review 1b

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { ERROR_CODE_NOT_FOUND } = require('./utils/constants');
 
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
@@ -7,7 +8,6 @@ const cardsRouter = require('./routes/cards');
 const app = express();
 
 const { PORT = 3000 } = process.env;
-const ERROR_CODE_NOT_FOUND = 404;
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
