c67a175ca193ccaf2cf2a0643ca2f103840083ac
Kirill Komarkov <koshmar1319@gmail.com>
2021-08-23 18:07:33 +0300

    add logger

--- app.js
+++ app.js
@@ -8,13 +8,14 @@ const { ERROR_CODE_NOT_FOUND } = require('./utils/constants');
 const { createUser, login } = require('./controllers/users');
 const { handleError, ErrorState } = require('./middlewares/errors');
 const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 
 const app = express();
 
-const { PORT = 3000 } = process.env;
+const { PORT, NODE_ENV } = process.env;
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -24,6 +25,13 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 
 app.use(express.json());
+app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
@@ -50,10 +58,10 @@ app.use((req, res, next) => {
   next(new ErrorState('Указанный путь не существует', ERROR_CODE_NOT_FOUND));
 });
 
+app.use(errorLogger);
+
 app.use(errors());
 
 app.use(handleError);
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
+app.listen(NODE_ENV === 'production' ? PORT : 3001);
