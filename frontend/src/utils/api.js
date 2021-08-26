1135eb84ca95741607116cd6f36aae2676bd8383
Kirill Komarkov <koshmar1319@gmail.com>
2021-08-26 13:23:33 +0300

    try to work api

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,25 +1,123 @@
+// class Api {
+//   constructor({ baseUrl, groupID, token, authUrl }) {
+//     this._baseUrl = baseUrl;
+//     this._groupID = groupID;
+//     this._token = token;
+//     this._authUrl = authUrl;
+//   }
+
+//   getUserInfo() {
+//     return fetch(`${this._baseUrl}/${this._groupID}/users/me`, {
+//       method: "GET",
+//       headers: {
+//         authorization: this._token,
+//       },
+//     }).then(this._showErrorMessage);
+//   }
+
+//   setUserInfo(data) {
+//     return fetch(`${this._baseUrl}/${this._groupID}/users/me`, {
+//       method: "PATCH",
+//       headers: {
+//         authorization: this._token,
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         name: data.name,
+//         about: data.about,
+//       }),
+//     }).then(this._showErrorMessage);
+//   }
+
+//   getInitialCards() {
+//     return fetch(`${this._baseUrl}/${this._groupID}/cards`, {
+//       method: "GET",
+//       headers: {
+//         authorization: this._token,
+//       },
+//     }).then(this._showErrorMessage);
+//   }
+
+//   addCard(card) {
+//     return fetch(`${this._baseUrl}/${this._groupID}/cards`, {
+//       method: "POST",
+//       headers: {
+//         authorization: this._token,
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         name: card.name,
+//         link: card.link,
+//       }),
+//     }).then(this._showErrorMessage);
+//   }
+
+//   deleteCard(card) {
+//     return fetch(`${this._baseUrl}/${this._groupID}/cards/${card._id}`, {
+//       method: "DELETE",
+//       headers: {
+//         authorization: this._token,
+//       },
+//     }).then(this._showErrorMessage);
+//   }
+
+//   setUserAvatar(avatar) {
+//     return fetch(`${this._baseUrl}/${this._groupID}/users/me/avatar`, {
+//       method: "PATCH",
+//       headers: {
+//         authorization: this._token,
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         avatar: avatar,
+//       }),
+//     }).then(this._showErrorMessage);
+//   }
+
+//   changeLikeCardStatus(card, likeCardStatus) {
+//     return fetch(`${this._baseUrl}/${this._groupID}/cards/likes/${card._id}`, {
+//       method: likeCardStatus ? "PUT" : "DELETE",
+//       headers: {
+//         authorization: this._token,
+//       },
+//     }).then(this._showErrorMessage);
+//   }
+
+//   _showErrorMessage = (res) => {
+//     if (res.ok) {
+//       return res.json();
+//     }
+//     return Promise.reject(`Ошибка ${res.status}`);
+//   };
+// }
+
+// const api = new Api({
+//   baseUrl: "https://mesto.nomoreparties.co/v1",
+//   groupID: "cohort-22",
+//   token: "23cb390e-6742-419b-8dec-601afe24420f",
+//   authUrl: "https://auth.nomoreparties.co",
+// });
+
+// export default api;
+
+//=========================================
+
 class Api {
-  constructor({ baseUrl, groupID, token, authUrl }) {
+  constructor(baseUrl) {
     this._baseUrl = baseUrl;
-    this._groupID = groupID;
-    this._token = token;
-    this._authUrl = authUrl;
   }
 
   getUserInfo() {
-    return fetch(`${this._baseUrl}/${this._groupID}/users/me`, {
-      method: "GET",
-      headers: {
-        authorization: this._token,
-      },
+    return fetch(`${this._baseUrl}/users/me`, {
+      credentials: 'include',
     }).then(this._showErrorMessage);
   }
 
   setUserInfo(data) {
-    return fetch(`${this._baseUrl}/${this._groupID}/users/me`, {
+    return fetch(`${this._baseUrl}/users/me`, {
       method: "PATCH",
+      credentials: 'include',
       headers: {
-        authorization: this._token,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
@@ -30,19 +128,16 @@ class Api {
   }
 
   getInitialCards() {
-    return fetch(`${this._baseUrl}/${this._groupID}/cards`, {
-      method: "GET",
-      headers: {
-        authorization: this._token,
-      },
+    return fetch(`${this._baseUrl}/cards`, {
+      credentials: 'include',
     }).then(this._showErrorMessage);
   }
 
   addCard(card) {
-    return fetch(`${this._baseUrl}/${this._groupID}/cards`, {
+    return fetch(`${this._baseUrl}/cards`, {
       method: "POST",
+      credentials: 'include',
       headers: {
-        authorization: this._token,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
@@ -53,19 +148,17 @@ class Api {
   }
 
   deleteCard(card) {
-    return fetch(`${this._baseUrl}/${this._groupID}/cards/${card._id}`, {
+    return fetch(`${this._baseUrl}/cards/${card._id}`, {
       method: "DELETE",
-      headers: {
-        authorization: this._token,
-      },
+      credentials: 'include',
     }).then(this._showErrorMessage);
   }
 
   setUserAvatar(avatar) {
-    return fetch(`${this._baseUrl}/${this._groupID}/users/me/avatar`, {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
       method: "PATCH",
+      credentials: 'include',
       headers: {
-        authorization: this._token,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
@@ -75,11 +168,9 @@ class Api {
   }
 
   changeLikeCardStatus(card, likeCardStatus) {
-    return fetch(`${this._baseUrl}/${this._groupID}/cards/likes/${card._id}`, {
+    return fetch(`${this._baseUrl}/cards/${card._id}/likes`, {
       method: likeCardStatus ? "PUT" : "DELETE",
-      headers: {
-        authorization: this._token,
-      },
+      credentials: 'include',
     }).then(this._showErrorMessage);
   }
 
@@ -92,10 +183,7 @@ class Api {
 }
 
 const api = new Api({
-  baseUrl: "https://mesto.nomoreparties.co/v1",
-  groupID: "cohort-22",
-  token: "23cb390e-6742-419b-8dec-601afe24420f",
-  authUrl: "https://auth.nomoreparties.co",
+  baseUrl: "http://api.kshmr-mesto.nomoredomains.monster",
 });
 
 export default api;
