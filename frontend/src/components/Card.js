a5543d4cc1f796bc19ab403b73c008be2b318f1f
Kirill Komarkov <koshmar1319@gmail.com>
2021-08-30 13:38:22 +0300

    fix delete card

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -5,7 +5,8 @@ import { CurrentUserContext } from "../contexts/CurrentUserContext";
 function Card({ card, onCardClick, onCardLike, onCardDelete }) {
   const currentUser = React.useContext(CurrentUserContext);
 
-  const isOwn = card.owner._id === currentUser._id;
+  // const isOwn = card.owner._id === currentUser._id;
+  const isOwn = card.owner === currentUser._id;
   const cardDeleteButtonClassName = `element__delete ${
     isOwn ? "" : "element__delete_inactive"
   }`;
